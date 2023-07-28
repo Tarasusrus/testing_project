3058137f8e5cef254b5e068f669e788721669157
tarasusrus <bar.norilsk@gmail.com>
2023-07-28 15:28:47 +0300

    Добавил докстринг

--- WEBTRONICS/models.py
+++ WEBTRONICS/models.py
@@ -2,16 +2,38 @@ from pydantic import BaseModel
 
 
 class Post(BaseModel):
+    """
+    Модель данных для поста (Post).
+
+    Attributes:
+        id (int): Идентификатор поста.
+        title (str): Заголовок поста.
+        content (str): Содержание поста.
+    """
     id: int
     title: str
     content: str
 
 
 class Like(BaseModel):
+    """
+    Модель данных для лайка (Like).
+
+    Attributes:
+        user_id (int): Идентификатор пользователя, который поставил лайк.
+        post_id (int): Идентификатор поста, которому был поставлен лайк.
+    """
     user_id: int
     post_id: int
 
 
 class Dislike(BaseModel):
+    """
+    Модель данных для дизлайка (Dislike).
+
+    Attributes:
+        user_id (int): Идентификатор пользователя, который поставил дизлайк.
+        post_id (int): Идентификатор поста, которому был поставлен дизлайк.
+    """
     user_id: int
     post_id: int
