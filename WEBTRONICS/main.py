8350047adeebc0a902c4d42bff2bf0009e55e6df
tarasusrus <bar.norilsk@gmail.com>
2023-07-28 15:27:37 +0300

    Добавил докстринг

--- WEBTRONICS/main.py
+++ WEBTRONICS/main.py
@@ -22,11 +22,23 @@ post_dislikes_cache = {}
 
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
+    """
+    Возвращает HTML-страницу с пользовательским интерфейсом Swagger UI.
+
+    Returns:
+        str: HTML-страница с интерфейсом Swagger UI.
+    """
     return get_swagger_ui_html(openapi_url="/openapi.json", title="Custom Swagger UI")
 
 
 @app.get("/openapi.json", include_in_schema=False)
 async def get_open_api_endpoint():
+    """
+    Возвращает JSON-схему OpenAPI для текущего приложения FastAPI.
+
+    Returns:
+        dict: JSON-схема OpenAPI для текущего приложения.
+    """
     return get_openapi(
         title="Custom OpenAPI",
         version="1.0.0",
@@ -36,6 +48,19 @@ async def get_open_api_endpoint():
 
 @app.post("/register")
 async def register_user(username: str, password: str):
+    """
+    Регистрирует нового пользователя с заданным именем и паролем.
+
+    Args:
+        username (str): Имя пользователя.
+        password (str): Пароль пользователя.
+
+    Returns:
+        dict: Словарь с сообщением об успешной регистрации.
+
+    Raises:
+        HTTPException: Если пользователь с таким именем уже существует (код 400).
+    """
     # Проверяем, что пользователь с таким именем не существует
     for user in users_db:
         if user["username"] == username:
@@ -50,6 +75,19 @@ async def register_user(username: str, password: str):
 
 @app.post("/login")
 async def login(username: str, password: str):
+    """
+    Аутентифицирует пользователя по заданному имени и паролю и возвращает JWT токен.
+
+    Args:
+        username (str): Имя пользователя.
+        password (str): Пароль пользователя.
+
+    Returns:
+        dict: Словарь с JWT токеном в случае успешной аутентификации.
+
+    Raises:
+        HTTPException: Если заданные учетные данные недействительны (код 401).
+    """
     # Ищем пользователя в "базе данных"
     for user in users_db:
         if user["username"] == username and user["password"] == password:
@@ -62,6 +100,18 @@ async def login(username: str, password: str):
 
 @app.post("/posts/", response_model=Post)
 async def create_post(post: Post):
+    """
+    Создает новый пост и добавляет его в базу данных.
+
+    Args:
+        post (Post): Модель данных для нового поста.
+
+    Returns:
+        model_dump: Словарь с данными нового поста, включая сгенерированный уникальный ID.
+
+    Raises:
+        HTTPException: Если не удалось создать пост или произошла ошибка при добавлении в базу данных.
+    """
     # Генерируем уникальный ID для нового поста
     new_post_id = max(post.id for post in posts_db) + 1 if posts_db else 1
     post_data = post.model_dump()
@@ -74,6 +124,18 @@ async def create_post(post: Post):
 
 @app.get("/posts/{post_id}", response_model=Post)
 async def read_post(post_id: int):
+    """
+    Возвращает данные о посте с указанным ID.
+
+    Args:
+        post_id (int): Идентификатор поста, который нужно найти.
+
+    Returns:
+        dict: Словарь с данными о посте, если он найден.
+
+    Raises:
+        HTTPException: Если пост с указанным ID не найден (ошибка 404).
+    """
     # Ищем пост с указанным ID в "базе данных"
     for post in posts_db:
         if post["id"] == post_id:
@@ -84,6 +146,19 @@ async def read_post(post_id: int):
 
 @app.put("/posts/{post_id}", response_model=Post)
 async def update_post(post_id: int, post: Post):
+    """
+    Обновляет данные о посте с указанным ID.
+
+    Args:
+        post_id (int): Идентификатор поста, который нужно обновить.
+        post (Post): Модель данных Post с новыми данными для обновления.
+
+    Returns:
+        dict: Словарь с обновленными данными о посте, если он найден.
+
+    Raises:
+        HTTPException: Если пост с указанным ID не найден (ошибка 404).
+    """
     # Ищем пост с указанным ID в "базе данных"
     for i, p in enumerate(posts_db):
         if p["id"] == post_id:
@@ -96,6 +171,18 @@ async def update_post(post_id: int, post: Post):
 
 @app.delete("/posts/{post_id}", response_model=Post)
 async def delete_post(post_id: int):
+    """
+    Удаляет пост с указанным ID из "базы данных".
+
+    Args:
+        post_id (int): Идентификатор поста, который нужно удалить.
+
+    Returns:
+        dict: Словарь с данными об удаленном посте, если он найден.
+
+    Raises:
+        HTTPException: Если пост с указанным ID не найден (ошибка 404).
+    """
     # Ищем пост с указанным ID в "базе данных" и удаляем его
     for i, post in enumerate(posts_db):
         if post["id"] == post_id:
@@ -107,6 +194,19 @@ async def delete_post(post_id: int):
 
 @app.post("/posts/{post_id}/like/", response_model=Like)
 async def like_post(post_id: int, like: Like):
+    """
+    Ставит лайк на пост с указанным ID и сохраняет лайк в кэше.
+
+    Args:
+        post_id (int): Идентификатор поста, на который нужно поставить лайк.
+        like (Like): Модель данных Like, содержащая информацию о лайке (например, ID пользователя).
+
+    Returns:
+        dict: Словарь с данными о поставленном лайке.
+
+    Raises:
+        HTTPException: Если пользователь пытается поставить лайк своему собственному посту (ошибка 400).
+    """
     # Проверяем, что пользователь не ставит лайк своему посту
     for post in posts_db:
         if post["id"] == post_id and post["user_id"] == like.user_id:
@@ -125,6 +225,19 @@ async def like_post(post_id: int, like: Like):
 
 @app.post("/posts/{post_id}/dislike/", response_model=Dislike)
 async def dislike_post(post_id: int, dislike: Dislike):
+    """
+    Ставит дизлайк на пост с указанным ID и сохраняет дизлайк в кэше.
+
+    Args:
+        post_id (int): Идентификатор поста, на который нужно поставить дизлайк.
+        dislike (Dislike): Модель данных Dislike, содержащая информацию о дизлайке (например, ID пользователя).
+
+    Returns:
+        dict: Словарь с данными о поставленном дизлайке.
+
+    Raises:
+        HTTPException: Если пользователь пытается поставить дизлайк своему собственному посту (ошибка 400).
+    """
     # Проверяем, что пользователь не ставит дизлайк своему посту
     for post in posts_db:
         if post["id"] == post_id and post["user_id"] == dislike.user_id:
@@ -143,6 +256,18 @@ async def dislike_post(post_id: int, dislike: Dislike):
 
 @app.get("/posts/{post_id}", response_model=Post)
 async def read_post(post_id: int):
+    """
+    Возвращает информацию о посте с указанным ID, включая количество лайков и дизлайков из кэша.
+
+    Args:
+        post_id (int): Идентификатор поста, информацию о котором нужно получить.
+
+    Returns:
+        dict: Словарь с данными о посте, включая количество лайков и дизлайков.
+
+    Raises:
+        HTTPException: Если пост с указанным ID не найден (ошибка 404).
+    """
     # Ищем пост с указанным ID в "базе данных"
     for post in posts_db:
         if post["id"] == post_id:
